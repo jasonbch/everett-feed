9e3b2f3a0ba259d91e91d8f51e246983e50f3bce
PHONG BACH <phongnghinhbach@gmail.com>
2020-02-02 05:57:27 -0800

    Updates

---
+++ precache-manifest.6dd392eeb4b57b644634e925a3b5c0d4.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75955e61960e969ee266d21a815db108",
+    "url": "/everett-feed/index.html"
+  },
+  {
+    "revision": "3f6d3d0cc9584c0b4bd8",
+    "url": "/everett-feed/static/css/2.5716b6af.chunk.css"
+  },
+  {
+    "revision": "76947eb4839728c58ca2",
+    "url": "/everett-feed/static/css/main.a7a096e5.chunk.css"
+  },
+  {
+    "revision": "3f6d3d0cc9584c0b4bd8",
+    "url": "/everett-feed/static/js/2.89df041f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/everett-feed/static/js/2.89df041f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76947eb4839728c58ca2",
+    "url": "/everett-feed/static/js/main.7382e12d.chunk.js"
+  },
+  {
+    "revision": "8786cb806a3cb51e90a5",
+    "url": "/everett-feed/static/js/runtime-main.78e0aedb.js"
+  },
+  {
+    "revision": "38f2f013d935d0859420990adf728d09",
+    "url": "/everett-feed/static/media/bg.38f2f013.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/everett-feed/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/everett-feed/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/everett-feed/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/everett-feed/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/everett-feed/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/everett-feed/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/everett-feed/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/everett-feed/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/everett-feed/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/everett-feed/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/everett-feed/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/everett-feed/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/everett-feed/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/everett-feed/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/everett-feed/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/everett-feed/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
